3b4b2bc076063885f3192f9a881ce20d6c07b462
kwang <ktkim@elicer.com>
2022-06-17 16:07:56 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { store } from './state';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <div>Hello!</div>
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>memberCheckPage</div>} />
+        <Route path="/login" element={<div>login</div>} />
+        <Route path="/register" element={<div>register</div>} />
+        <Route path="/spaceRegister" element={<div>pageRegister</div>} />
+        <Route path="/:space" element={<div>myspace</div>} />
+        <Route path="/:space/design" element={<div>myspaceDesignMode</div>} />
+        <Route path="/:space/:page" element={<div>myspace/page</div>} />
+        <Route
+          path="/:space/:page/design"
+          element={<div>myspace/pageDesignMode</div>}
+        />
+      </Routes>
+    </Router>
+  </Provider>
 );
